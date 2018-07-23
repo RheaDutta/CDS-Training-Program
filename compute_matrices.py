f9caeb7d9dfed6102257e4eca591277d64151a2d
Rhea Dutta <rheadutta2468@gmail.com>
2018-07-23 14:12:36 -0700

    Completed the script.

--- compute_matrices.py
+++ compute_matrices.py
@@ -1,14 +1,5 @@
 import generate_matrix
 
-#Test case
-mat = [[2,0],[0,0]] #10 states
-num_range = [0,2]
-
-#Generating the P-Matrix and the Reduced Matrix.
-result = generate_matrix.compute(mat, num_range)
-p_matrix = result[0]
-r_matrix = result[1]
-
 ##################################################################
 
 def compute_probability_matrix(p_matrix):
@@ -17,12 +8,14 @@ def compute_probability_matrix(p_matrix):
 	
 	prob_matrix = []
 	for result in p_matrix:
-		p = 0
+		row = []
 		for prob in result:
+			p = 0
 			for i in range(len(prob)):
 				if prob[i]!=0:
 					p = p + (1/prob[i])
-		prob_matrix.append(p)
+			row.append(p)
+		prob_matrix.append(row)
 
 	return prob_matrix
 
@@ -35,52 +28,78 @@ def compute_reduced_matrix(r_matrix):
 	result = []
 	
 	#Compressing sum.
-	for element in r_matrix:
-		n = element[0]
-		prob = element[1]
-		sum = 0
-		for l in prob:
-			for i in range(len(l)):
-				if l[i]!=0:
-					sum+=(1/l[i])	
-		result.append((n,sum))
+	for list in r_matrix:
+		new_list = []
+		for tup in list:
+			new_tup = compute_reduced_matrix_helper(tup)
+			new_list.append(new_tup)
+		result.append(new_list)
 	
 	reduced_matrix = []
 	
 	#Compressing further.
-	for element in result:
-		reduced_matrix.append(element[1]/element[0])
+	for list in result:
+		row = []
+		for tup in list:
+			row.append(tup[1]/tup[0])
+		reduced_matrix.append(row)
 		
 	return reduced_matrix
 
 ##################################################################
 
-def printing(l):
+def compute_reduced_matrix_helper(tup):
 
-	pass
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	n = tup[0]
+	p = tup[1]
+	sum = 0
+	for p_list in p:
+		for m in p_list:
+			if m!=0:
+				sum+=(1/m)
+	
+	return (n,sum)
 
 ##################################################################
-#Computing the P-Matrix.
-probability_matrix = compute_probability_matrix(p_matrix)
-print("P-Matrix: ", probability_matrix)
-#Computing the reduced matrix.
-reduced_matrix = compute_reduced_matrix(r_matrix)
-print("Reduced Matrix: ", reduced_matrix)
 
+def printing(l):
 
+	print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+	for list in l:
+		print(list)
+		print("----------------------------------------")
+	print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+	
+##################################################################
+
+def do():
+	
+	#Test case
+	#mat = [[2,0],[0,0]] #10 states
+	#num_range = [0,2]
+	
+	mat = [[2,3],[0,0]] #56 states
+	num_range = [0,5]
+	
+	#Generating the P-Matrix and the Reduced Matrix.
+	result = generate_matrix.compute(mat, num_range)
+	p_matrix = result[0]
+	r_matrix = result[1]
+	
+	#Computing the P-Matrix.
+	probability_matrix = compute_probability_matrix(p_matrix)
+	
+	#Computing the reduced matrix.
+	reduced_matrix = compute_reduced_matrix(r_matrix)
+	
+	#Printing both matrices.
+	#print("PROBABILITY MATRIX: ")
+	#printing(probability_matrix)
+	print("REDUCED PROBABILITY MATRIX: ")
+	printing(reduced_matrix)
+	
+##################################################################
+
+#Executing the script.
+do()
 		

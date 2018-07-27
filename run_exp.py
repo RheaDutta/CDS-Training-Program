7001542bd3aad22ccc12cc14d94b4f5c21ffb4c5
Rhea Dutta <rheadutta2468@gmail.com>
2018-07-27 13:16:42 -0700

    Added instructions.

--- run_exp.py
+++ run_exp.py
@@ -3,14 +3,35 @@
 DOCUMENTATION:
 
 This script has multiple functions - 
-    -> It generates the probability matrix for the given input.
-    -> It generates the reduced probability matrix for the given input.
+    -> It can generate the probability matrix for the given input.
+    -> It can generate the reduced probability matrix for the given input.
         (This is essentially a condensed version of the probability matrix)
-    -> It finds the bounds on the mixing time for each of the aforementioned matrices.
+    -> It can find the bounds on the mixing time for each of the aforementioned matrices.
 
 THe program has the following functions - 
-    ->
-    ->
+    -> run_experiments()
+    -> only_matrices()
+    -> matrices_and_bounds()
+    -> only_bounds()
+    -> find_input()
+    -> store_output()
+
+Instructions for running the script -
+
+    1. Right under this documentation, a few scripts have been imported. 
+        -> Choose one of generate_matrix and pMatrix_main_mp.
+        -> They have the same output.
+        -> generate_matrix runs sequentially.
+        -> pMatrix_main_mp uses multiprocessing.
+
+    2. Edit run_experiments() to choose which experiment to run.
+
+    3. Scroll to the bottom of the script. If you would like your results to be printed to the screen,
+        change must_print to True. Otherwise, change it to False. In either case, the results 
+        will be stored.
+
+    4. Change the input in find_input().
+
 
 Written by Rhea Dutta
 

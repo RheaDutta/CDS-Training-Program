5f3bf08f7317142395af410909b3312a861109e4
Akshith Gunasekaran <gunaseka@oregonstate.edu>
2018-07-29 22:23:13 -0700

    refactor

--- bound.py
+++ bound.py
@@ -4,26 +4,26 @@ DOCUMENTATION:
 This script computes the bound on the mixing time for a Probability Matrix.
 
 Functions in the program -
-	-> calculate_bound()
-	-> convert_matrix()
-	-> convert_to_pari()
-	-> printing_bound()
-	-> printing_matrix()
-	-> printing_summary()
-	-> execute_script()
-	
+    -> calculate_bound()
+    -> convert_matrix()
+    -> convert_to_pari()
+    -> printing_bound()
+    -> printing_matrix()
+    -> printing_summary()
+    -> execute_script()
+
 Two scripts - generate_matrix and pMatrix_main_mp - are imported to generate the
 P-Matrix and the reduced P-Matrix. (generate_matrix is sequentially computed,
 while pMatrix_main_mp is optimized using multiprocessing. Use either one.)
 
 Output of both scripts is in the format - (P-Matrix, Reduced Matrix)
-	-> Format of P-Matrix -
-			-> 3D list with inner most elements: [numerator, denominator]
-			-> The element is simplified this way - numerator//denominator
-	-> Format of reduced P-Matrix
-			-> 2D list with inner most elements that are tuples in the form:
-				(n, numerator, denominator)
-			-> The element is simplified this way - (numerator//denominator)//n
+    -> Format of P-Matrix -
+            -> 3D list with inner most elements: [numerator, denominator]
+            -> The element is simplified this way - numerator//denominator
+    -> Format of reduced P-Matrix
+            -> 2D list with inner most elements that are tuples in the form:
+                (n, numerator, denominator)
+            -> The element is simplified this way - (numerator//denominator)//n
 
 calculate_bound() originally written by Kimia Tajik in Pari/GP.
 Script written and implemented by Rhea Dutta in Python.
@@ -42,13 +42,15 @@ pari = CP.Pari() #Pari object.
 
 import math
 
+# Set True to print
+verbose = True
 #______________________________________________________________________________________________#
 
 def calculate_bound():#matrix, is_p_matrix):
 
 	"""
-	
-	RETURNS: The bound on mixing time for the given matrix. 
+
+	RETURNS: The bound on mixing time for the given matrix.
 
 	PARAMETERS: matrix: The matrix for which the bound on the mixing time must be calculated.
 						(The input is the output of the generate_matrix / pMatrix_main_mp script)
@@ -69,10 +71,10 @@ def calculate_bound():#matrix, is_p_matrix):
 	epsilon = pari('80')
 	Size = pari.matsize(P)
 	N = pari(Size[1])
-	
+
 	D = pari(1/N)*pari.matid(N)
 	#D = (1/N)*pari.matid(N) #this was there before the change
-	
+
 	T = pari.mattranspose(P)
 	X = pari.matinverseimage(D,T)
 	B = X*D
@@ -86,219 +88,218 @@ def calculate_bound():#matrix, is_p_matrix):
 	#sle = L[N-2] #this was there before the change
 
 	bound = -2 * (math.log(2)/math.log(sle)) * (epsilon + math.log(N-1)/math.log(2))
-	
+
 	bound = math.ceil(bound)
-	
+
 	return bound
 #______________________________________________________________________________________________#
 
 def convert_matrix(matrix, is_p_matrix):
 
-	"""
-	RETURNS: The Pari.t_MAT form of the P-Matrix.
+    """
+    RETURNS: The Pari.t_MAT form of the P-Matrix.
 
-	PARAMETERS: matrix [3D list]: The matrix for which the bound on mixing time must be found.
-									(Basically, it is the output of the aforementioned scripts.)
-				is_p_matrix [bool]: True if given matrix is a P-Matrix, False otherwise.
+    PARAMETERS: matrix [3D list]: The matrix for which the bound on mixing time must be found.
+                                    (Basically, it is the output of the aforementioned scripts.)
+                is_p_matrix [bool]: True if given matrix is a P-Matrix, False otherwise.
 
-	EXAMPLE: input = [[[1,2], [3,4]], [[5,6],[7,8]]]
-			output = [1/2,3/4;5/6,7/8] wherein the output is a t_MAT and each element is a t_FRAC.
+    EXAMPLE: input = [[[1,2], [3,4]], [[5,6],[7,8]]]
+            output = [1/2,3/4;5/6,7/8] wherein the output is a t_MAT and each element is a t_FRAC.
 
-	"""
+    """
 
-	#Conversion for P-Matrix.
-	if is_p_matrix:
-		new_matrix = []
-		for row in matrix:
-			new_row = []
-			for prob in row:
-				if prob[0]!=0:
-					num = pari(prob[0])
-					den = pari(prob[1])
-					new_prob = num/den
-					new_row.append(new_prob)
-				else:
-					new_row.append(pari('0'))
-			new_matrix.append(new_row)
+    #Conversion for P-Matrix.
+    if is_p_matrix:
+        new_matrix = []
+        for row in matrix:
+            new_row = []
+            for prob in row:
+                if prob[0]!=0:
+                    num = pari(prob[0])
+                    den = pari(prob[1])
+                    new_prob = num/den
+                    new_row.append(new_prob)
+                else:
+                    new_row.append(pari('0'))
+            new_matrix.append(new_row)
 
-		result = convert_to_pari(new_matrix)
-		return result
+        result = convert_to_pari(new_matrix)
+        return result
 
-	#Conversion for reduced P-Matrix. 
-	else:
-		new_matrix = []
-		for row in matrix:
-			new_row = []
-			for prob in row:
-				if prob[1]!=0:
-					n = pari(prob[0])
-					num = pari(prob[1])
-					den = pari(prob[2])
-					new_prob = num/(den*n)
-					new_row.append(new_prob)
-				else:
-					new_row.append(pari('0'))
-			new_matrix.append(new_row)
+    #Conversion for reduced P-Matrix.
+    else:
+        new_matrix = []
+        for row in matrix:
+            new_row = []
+            for prob in row:
+                if prob[1]!=0:
+                    n = pari(prob[0])
+                    num = pari(prob[1])
+                    den = pari(prob[2])
+                    new_prob = num/(den*n)
+                    new_row.append(new_prob)
+                else:
+                    new_row.append(pari('0'))
+            new_matrix.append(new_row)
 
-		result = convert_to_pari(new_matrix)
-		return result
+        result = convert_to_pari(new_matrix)
+        return result
 #______________________________________________________________________________________________#
 
 def convert_to_pari(matrix):
 
-	"""
-	RETURNS: The Pari.t_MAT form of the P-Matrix.
+    """
+    RETURNS: The Pari.t_MAT form of the P-Matrix.
 
-	PARAMETERS: matrix [2D list]: The matrix for which the bound on mixing time must be found.
+    PARAMETERS: matrix [2D list]: The matrix for which the bound on mixing time must be found.
 
-	EXAMPLE: input = [[1/2, 3/4], [5/6, 7/8]] wherein each element is a t_FRAC.
-			output = [1/2,3/4;5/6,7/8] wherein the output is a t_MAT. 
+    EXAMPLE: input = [[1/2, 3/4], [5/6, 7/8]] wherein each element is a t_FRAC.
+            output = [1/2,3/4;5/6,7/8] wherein the output is a t_MAT.
 
-	"""
+    """
 
-	new_matrix = ''
+    new_matrix = ''
 
-	for i in range(len(matrix)):
-		new_row = ''
+    for i in range(len(matrix)):
+        new_row = ''
 
-		for j in range(len(matrix[i])):
-			
-			if j==len(matrix[i])-1:
-				last = ''
-			else:
-				last = ','
-			new_row = new_row + str(matrix[i][j]) + last
-		
-		if i==len(matrix)-1:
-			final = ''
-		else:
-			final = ';'
-		new_matrix = new_matrix + new_row + final
-	
-	r = '['+ new_matrix + ']'
-	result = pari(r)
-	
-	return result
+        for j in range(len(matrix[i])):
+
+            if j==len(matrix[i])-1:
+                last = ''
+            else:
+                last = ','
+            new_row = new_row + str(matrix[i][j]) + last
+
+        if i==len(matrix)-1:
+            final = ''
+        else:
+            final = ';'
+        new_matrix = new_matrix + new_row + final
+
+    r = '['+ new_matrix + ']'
+    result = pari(r)
+
+    return result
 #______________________________________________________________________________________________#
 
 def printing_bound(n, is_p_matrix):
 
-	"""
+    """
 
-	Prints the given bound.
-	
-	PARAMETERS: n [int/float]: The given bound.
-				is_p_matrix [bool]: True if n is a bound on the mixing time for a P-Matrix, False
-								otherwise.  
+    Prints the given bound.
 
-	"""
+    PARAMETERS: n [int/float]: The given bound.
+                is_p_matrix [bool]: True if n is a bound on the mixing time for a P-Matrix, False
+                                otherwise.
 
-	print("____________________________________________________________________________________")
-	if is_p_matrix:
-		print("Bound on mixing time for P-Matrix: ", n)
-	else:
-		print("Bound on mixing time for reduced P-Matrix: ", n)
-	print("____________________________________________________________________________________")
+    """
+
+    print("____________________________________________________________________________________")
+    if is_p_matrix:
+        print("Bound on mixing time for P-Matrix: ", n)
+    else:
+        print("Bound on mixing time for reduced P-Matrix: ", n)
+    print("____________________________________________________________________________________")
 #______________________________________________________________________________________________#
 
 def printing_matrix(matrix, is_p_matrix):
 
-	"""
+    """
 
-	Prints the given matrix.
-	
-	PARAMETERS: matrix [Pari.t_MAT]: The given matrix.
-				is_p_matrix [bool]: True if matrix is a P-Matrix, False
-								otherwise.  
+    Prints the given matrix.
 
-	"""	
+    PARAMETERS: matrix [Pari.t_MAT]: The given matrix.
+                is_p_matrix [bool]: True if matrix is a P-Matrix, False
+                                otherwise.
 
-	#For P-Matrix.
-	if is_p_matrix:
-		print("________________________________PROBABILITY MATRIX__________________________________ ")
+    """
 
-	#For reduced P-Matrix.
-	else:
-		print("________________________________REDUCED MATRIX______________________________________")
-	
-	
-	for i in range(len(matrix)):
-		print(matrix[i])
-		print("-------------------------------------------------------------------------------------")
-	print("____________________________________________________________________________________")
+    #For P-Matrix.
+    if is_p_matrix:
+        print("________________________________PROBABILITY MATRIX__________________________________ ")
+
+    #For reduced P-Matrix.
+    else:
+        print("________________________________REDUCED MATRIX______________________________________")
+
+
+    for i in range(len(matrix)):
+        print(matrix[i])
+        print("-------------------------------------------------------------------------------------")
+    print("____________________________________________________________________________________")
 #______________________________________________________________________________________________#
 
 def printing_summary(p_matrix, r_matrix):
 
-	"""
+    """
 
-	Prints summary of data.
+    Prints summary of data.
 
-	"""
+    """
 
-	print("________________________________SUMMARY OF DATA_____________________________________")
-	print(" 1. P-Matrix")
-	print("		-> Number of rows: ", len(p_matrix))
+    print("________________________________SUMMARY OF DATA_____________________________________")
+    print(" 1. P-Matrix")
+    print("        -> Number of rows: ", len(p_matrix))
 
-	s = 0
-	for p in p_matrix[0]:
-		s+=1
-	
-	print("		-> Number of columns: ", s)
+    s = 0
+    for p in p_matrix[0]:
+        s+=1
+
+    print("        -> Number of columns: ", s)
 
 
-	print(" 2. Reduced P-Matrix")
-	print("		-> Number of super states: ", len(r_matrix))
-	
-	p = 0
-	for row in r_matrix:
-		p+=len(row)
-		
-	print("		-> Total number of sub states: ", p)
-	print("____________________________________________________________________________________")
+    print(" 2. Reduced P-Matrix")
+    print("        -> Number of super states: ", len(r_matrix))
+
+    p = 0
+    for row in r_matrix:
+        p+=len(row)
+
+    print("        -> Total number of sub states: ", p)
+    print("____________________________________________________________________________________")
 #______________________________________________________________________________________________#
 
-def execute_script(input, must_print, only_bounds):
+def execute_script(input, only_bounds):
 
-	"""
-	
-	Executes the script.
-	Output in the form [P_MATRIX, R_MATRIX, P_BOUND, R_BOUND].
+    """
 
-	PARAMETER: input [list]: [matrix, range]
-				must_print [bool]: True if results must be printed, False otherwise.
-				only_bounds [bool]: True if only bounds required and not matrices.
-									False otherwise.
+    Executes the script.
+    Output in the form [P_MATRIX, R_MATRIX, P_BOUND, R_BOUND].
 
-	"""
+    PARAMETER: input [list]: [matrix, range]
+                only_bounds [bool]: True if only bounds required and not matrices.
+                                    False otherwise.
 
-	#Comment out whichever one is not being used.
-	#Do not change 'False' here.
-	matrices = GM.compute(input, False)
-	#matrices = PM.compute(input, False)
+    """
 
-	#The required matrices. 
-	P_MATRIX = matrices[0]
-	R_MATRIX = matrices[1]
+    #Comment out whichever one is not being used.
+    #Do not change 'False' here.
+    matrices = GM.compute(input, False)
+    #matrices = PM.compute(input, False)
 
-	#Executing the script.
-	P_BOUND = calculate_bound(P_MATRIX, True)
-	R_BOUND = calculate_bound(R_MATRIX, False)
-	
-	#Printing results.
-	if must_print:
-		if only_bounds:
-			printing_bound(P_BOUND, True)
-			printing_bound(R_BOUND, False)
-		else:
-			printing_matrix(P_MATRIX, True)
-			printing_matrix(R_MATRIX, False)
-			printing_bound(P_BOUND, True)
-			printing_bound(R_BOUND, False)
-			printing_summary(P_MATRIX, R_MATRIX)
+    #The required matrices.
+    P_MATRIX = matrices[0]
+    R_MATRIX = matrices[1]
 
-	#Returning results.
-	return [P_MATRIX, R_MATRIX, P_BOUND, R_BOUND]
+    #Executing the script.
+    P_BOUND = calculate_bound(P_MATRIX, True)
+    R_BOUND = calculate_bound(R_MATRIX, False)
+
+    #Printing results.
+    if verbose:
+        if only_bounds:
+            printing_bound(P_BOUND, True)
+            printing_bound(R_BOUND, False)
+        else:
+            printing_matrix(P_MATRIX, True)
+            printing_matrix(R_MATRIX, False)
+            printing_bound(P_BOUND, True)
+            printing_bound(R_BOUND, False)
+            printing_summary(P_MATRIX, R_MATRIX)
+
+    #Returning results.
+    return [P_MATRIX, R_MATRIX, P_BOUND, R_BOUND]
 #______________________________________________________________________________________________#
 
 #input = [[[2,0],[0,0]], [0,2]]
--- run_exp.py
+++ run_exp.py
@@ -2,13 +2,13 @@
 
 DOCUMENTATION:
 
-This script has multiple functions - 
+This script has multiple functions -
     -> It can generate the probability matrix for the given input.
     -> It can generate the reduced probability matrix for the given input.
         (This is essentially a condensed version of the probability matrix)
     -> It can find the bounds on the mixing time for each of the aforementioned matrices.
 
-THe program has the following functions - 
+THe program has the following functions -
     -> run_experiments()
     -> only_matrices()
     -> matrices_and_bounds()
@@ -18,7 +18,7 @@ THe program has the following functions -
 
 Instructions for running the script -
 
-    1. Right under this documentation, a few scripts have been imported. 
+    1. Right under this documentation, a few scripts have been imported.
         -> Choose one of generate_matrix and pMatrix_main_mp.
         -> They have the same output.
         -> generate_matrix runs sequentially.
@@ -27,166 +27,66 @@ Instructions for running the script -
     2. Edit run_experiments() to choose which experiment to run.
 
     3. Scroll to the bottom of the script. If you would like your results to be printed to the screen,
-        change must_print to True. Otherwise, change it to False. In either case, the results 
+        change must_print to True. Otherwise, change it to False. In either case, the results
         will be stored.
 
     4. Change the input in find_input().
 
 
 Written by Rhea Dutta
-
-07/26/2018
-
 """
-#___________________________________________________________________________________________
+import sys
+import bound
+import pMatrix_main_mp as M
+# import generate_matrix as M
+from file_to_matrix import parse_matrix
 
-#To find matrices + bounds.
-import bound 
 
-#To find only matrices. Use either one.
-import generate_matrix as M 
-#import pMatrix_main_mp as M
-
-#___________________________________________________________________________________________
-
-def run_experiments(must_print):
-
-    """
-    
-    Runs experiments. Use whichever one is required.
-
-    PARAMETERS: must_print[bool]: True if result should be printed, False otherwise.
-
-    """
-
-    #Only generate matrices.
-    only_matrices(must_print)
-    
-    #Both matrices and bounds.
-    #matrices_and_bounds(must_print)
-
-    #Only bounds
-    #only_bounds(must_print)
-#___________________________________________________________________________________________
-
-def only_matrices(must_print):
-
+def generate_pmatrix(inputs):
     """
     Generates P-Matrix and reduced P-Matrix.
     Output in format - [p_matrix, reduced p_matrix].
-
-    PARAMETERS: must_print[bool]: True if result should be printed, False otherwise.
-
     """
-
-    #Extracting input.
-    input = find_input()
 
     #Generates matrices. matrices = [p_matrix, reduced p_matrix]
-    matrices = M.compute(input, must_print)
-    
-    #Storing output.
-    store_output(matrices)
-#___________________________________________________________________________________________
+    matrices = M.compute(inputs)
+    return matrices
 
-def matrices_and_bounds(must_print):
-
-    """
-    Generates P-Matrix, reduced P-Matrix, and bounds on mixing time for both.
-    Output in format - [P_MATRIX, R_MATRIX, P_BOUND, R_BOUND]
-
-    PARAMETERS: must_print[bool]: True if result should be printed, False otherwise.
-
-    """
-
-    #Extracting input.
-    input = find_input()
-
-    #Running the experiment.
-    result = bound.execute_script(input, must_print, False)
-    
-    #Storing output.
-    store_output(result) #result = [P_MATRIX, R_MATRIX, P_BOUND, R_BOUND]
-#___________________________________________________________________________________________
-
-def only_bounds(must_print):
-
+def generate_bounds(inputs):
     """
     Generates the bounds on mixing time for the P-matrix and the reduced P-Matrix.
     Output in format - [P_BOUND, R_BOUND].
-
-    PARAMETERS: must_print[bool]: True if result should be printed, False otherwise.
-
     """
 
-    #Extracting input.
-    input = find_input()
+    bounds = bound.execute_script(inputs, True)[2:]
+    return bounds
 
-    #Running the experiment.
-    result = bound.execute_script(input, must_print, True)[2:]
-    
-    #Storing output.
-    store_output(result) #result = [P_BOUND, R_BOUND]
-#___________________________________________________________________________________________
 
-def find_input():
-
+def get_input(file):
     """
-    Extract input from some file.
-
-
-
+    Extract input from sample_inputs.
     """
-
-    mat = [[2,0],[0,0]] #7 states
-    num_range = [0,1]
-
-    #mat = [[2,3],[0,0]] #56 states
-    #num_range = [0,5]
-
-    #mat = [[5,0],[0,0]] #41 states
-    #num_range = [0,3]
-
-    #mat = [[3,0],[0,0]] #17 states
-    #num_range = [0,2]
-
-    #mat = [[2,0],[0,0]] #10 states
-    #num_range = [0,2]
-
-    #mat = [[2,1],[0,0]] #20 states
-    #num_range = [0,3]
-
-    #mat = [[7,7],[0,0]]
-    #num_range = [0,7]
-    
-    #mat = [[2,3],[1,0]] #44 states
-    #num_range = [0,3]
-
-    #mat = [[0,6],[0,0]] #84 states
-    #num_range = [0,6]
-
-    #mat = [[3,0],[0,0]] #17 states
-    #num_range = [0,2]
-
-    input = [mat, num_range]
-
-    return input
-#___________________________________________________________________________________________
+    inputs = parse_matrix(file)
+    return inputs
 
 def store_output(output):
-
     """
     Store output in some file.
-
-
-
     """
-
     pass
-#___________________________________________________________________________________________
 
-#If results should be printed, must_print = True. False otherwise.
-must_print = True
 
-#Running the experiment. Open the function to pick which one to run. 
-run_experiments(must_print)
+def usage():
+    print('Usage: ',sys.argv[0]," filename")
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        usage()
+        exit(0)
+
+    file = sys.argv[1]
+    inputs = get_input(file)
+
+    for mat, num_range in inputs:
+        generate_pmatrix([mat, num_range])
+        generate_bounds([mat, num_range])
